ab0b71d24906f3f866050a6c269218b20ecfdaa5
Aksel Borgen <akselborgen@outlook.com>
2020-12-03 15:01:32 +0100

    Initial commit

---
+++ options/portfolio.py
@@ -0,0 +1,123 @@
+import graphviz
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import List
+
+PUT = +1
+CALL = -1
+
+
+def union(rng1, rng2):
+    (l1, h1) = rng1
+    (l2, h2) = rng2
+    return min(l1, l2), max(h1, h2)
+
+
+class Option:
+    def __init__(self, kind: int, strike_price: int, price: int = 0):
+        self.kind = kind
+        self.strike_price = strike_price
+        self.price = price
+
+    def __repr__(self):
+        if self.kind == PUT:
+            return f'Put(strike = {self.strike_price})'
+        elif self.kind == CALL:
+            return f'Call(strike = {self.strike_price})'
+
+    def range_of_interest(self):
+        return (self.strike_price, self.strike_price)
+
+    def value_at(self, stock_price: int):
+        return np.maximum(self.kind * (self.strike_price - stock_price), 0)
+
+    def profit_at(self, stock_price: int):
+        return self.value_at(stock_price) - self.price
+
+    def _repr_png_(self, fig_ax=None, fig_low=None, fig_high=None, linestyle='-', labels=None):
+        fig, ax = plt.subplots() if fig_ax is None else fig_ax
+        labels = [] if labels is None else labels
+
+        low, high = self.range_of_interest()
+        if fig_low is None:
+            fig_low = low - max(10, 0.1*(high - low))
+        if fig_high is None:
+            fig_high = high + max(10, 0.1*(high - low))
+
+        x = np.linspace(fig_low, fig_high, 100)
+        y = self.value_at(x)
+        labels.append(self.__repr__())
+
+        ax.plot(x, y, linestyle)
+
+        if fig_ax is None:
+            fig.legend(labels)
+            return display(fig)
+
+
+class Put(Option):
+    def __init__(self, strike_price: int, price: int = 0):
+        super().__init__(PUT, strike_price, price)
+
+
+class Call(Option):
+    def __init__(self, strike_price: int, price: int = 0):
+        super().__init__(CALL, strike_price, price)
+
+
+class Short(Option):
+    def __init__(self, option: Option):
+        self.option = option
+
+    def value_at(self, stock_price: int):
+        return -self.option.value_at(stock_price)
+
+    def profit_at(self, stock_price: int):
+        return self.value_at(stock_price) + self.option.price
+
+    def range_of_interest(self):
+        return self.option.range_of_interest()
+
+    def __repr__(self):
+        return f'Short({self.option})'
+
+    @property
+    def strike_price(self):
+        return self.option.strike_price
+
+
+class Portfolio(Option):
+    def __init__(self, *options: List[Option]):
+        self.options = options
+
+    def __repr__(self):
+        return 'Portfolio'
+
+    def range_of_interest(self):
+        rng = self.options[0].range_of_interest()
+        for o in self.options:
+            rng = union(rng, o.range_of_interest())
+        return rng
+
+    def value_at(self, stock_price):
+        return sum(o.value_at(stock_price) for o in self.options)
+
+    def _repr_png_(self, fig_ax=None, fig_low=None, fig_high=None, linestyle='-', labels=None):
+        (fig, ax) = plt.subplots() if fig_ax is None else fig_ax
+        labels = [] if labels is None else labels
+
+        low, high = self.range_of_interest()
+        if fig_low is None:
+            fig_low = low - max(10, 0.1*(high - low))
+        if fig_high is None:
+            fig_high = high + max(10, 0.1*(high - low))
+
+        for option in self.options:
+            option._repr_png_((fig, ax), fig_low, fig_high,
+                              linestyle='--', labels=labels)
+
+        super()._repr_png_((fig, ax), fig_low, fig_high, linestyle, labels)
+
+        if fig_ax is None:
+            fig.legend(labels)
+            return display(fig)
---
+++ options/pricing.py
@@ -0,0 +1,106 @@
+import graphviz
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import List
+
+
+def node_id(stock_price, period):
+    return f'{period}_{stock_price}'
+
+
+PUT = +1
+CALL = -1
+
+
+def current_value(stock_price, strike_price, option_type):
+    return max(option_type * (strike_price - stock_price), 0)
+
+
+def binomial_pricing(stock_price, strike_price, option_type, risk_free_rate, up, down, periods=1):
+    dot = graphviz.Digraph()
+    dot.graph_attr['rankdir'] = 'LR'
+    _binomial_pricing(stock_price, strike_price, option_type,
+                      risk_free_rate, up, down, {}, dot, periods=1, current_period=0)
+    return dot
+
+
+def _binomial_pricing(stock_price, strike_price, option_type, risk_free_rate, up, down, memo=None, dot=None, periods=1, current_period=0):
+    if current_period == periods:
+        value = current_value(stock_price, strike_price, option_type)
+        dot.node(node_id(stock_price, current_period),
+                 f'STOCK = {stock_price:.4g}\n––––––––––––\nvalue = {value:.4g}')
+        return value
+
+    if (current_period, stock_price) in memo:
+        return memo[(current_period, stock_price)]
+
+    up_price = stock_price * up
+    up_value = _binomial_pricing(up_price, strike_price, option_type,
+                                 risk_free_rate, up, down, memo, dot, periods, current_period + 1)
+
+    down_price = stock_price * down
+    down_value = _binomial_pricing(down_price, strike_price, option_type,
+                                   risk_free_rate, up, down, memo, dot, periods, current_period + 1)
+
+    # We want up_price * S + B(1 + rf) = up_value
+    # and down_price * S + B(r + rf) = down_value
+    # => (up_price - down_price)S = up_value - down_value
+    # => S = (up_value - down_value) / (up_price - down_price)
+    S = (up_value - down_value) / (up_price - down_price)
+    B = (up_value - up_price * S) / (1 + risk_free_rate)
+    value = stock_price * S + B
+    memo[(current_period, stock_price)] = value
+
+    dot.node(node_id(stock_price, current_period),
+             f'STOCK = {stock_price}\n––––––––––––\nvalue = {value:.4g}\nS = {S:.4g}\nB = {B:.4g}')
+    dot.edge(node_id(stock_price, current_period),
+             node_id(up_price, current_period + 1))
+    dot.edge(node_id(stock_price, current_period),
+             node_id(down_price, current_period + 1))
+
+    return value
+
+
+def _risk_neutral_probabilities(stock_price, strike_price, option_type, risk_free_rate, up, down, memo, dot, periods=1, current_period=0):
+    if current_period == periods:
+        value = current_value(stock_price, strike_price, option_type)
+        dot.node(node_id(stock_price, current_period),
+                 f'STOCK = {stock_price:.4g}\n––––––––––––\nvalue = {value:.4g}')
+        return value
+
+    if (current_period, stock_price) in memo:
+        return memo[(current_period, stock_price)]
+
+    up_price = stock_price * up
+    up_value = _risk_neutral_probabilities(
+        up_price, strike_price, option_type, risk_free_rate, up, down, memo, dot, periods, current_period + 1)
+
+    down_price = stock_price * down
+    down_value = _risk_neutral_probabilities(
+        down_price, strike_price, option_type, risk_free_rate, up, down, memo, dot, periods, current_period + 1)
+
+    # stock today = (p up_price + (1 - p) down_price) / (1 + r_f)
+    # (1 + rf) today = p up + down - p down
+    # p (up - down) = (1 + rf) today - down
+    # p = [(1 + rf) today - down] / (up - down)
+    p = ((1 + risk_free_rate) * stock_price -
+         down_price) / (up_price - down_price)
+    value = (p * up_value + (1 - p) * down_value) / (1 + risk_free_rate)
+    memo[(current_period, stock_price)] = value
+
+    dot.node(node_id(stock_price, current_period),
+             f'STOCK = {stock_price}\n––––––––––––\nvalue = {value:.4g}\np = {p:.4g}')
+    dot.edge(node_id(stock_price, current_period),
+             node_id(up_price, current_period + 1), 'p')
+    dot.edge(node_id(stock_price, current_period), node_id(
+        down_price, current_period + 1), '1 - p')
+
+    return value
+
+
+def risk_neutral_probabilities(stock_price, strike_price, option_type, risk_free_rate, up, down, periods=1):
+    dot = graphviz.Digraph()
+    dot.graph_attr['rankdir'] = 'LR'
+    _risk_neutral_probabilities(stock_price, strike_price, option_type,
+                                risk_free_rate, up, down, {}, dot, periods=1, current_period=0)
+    return dot
